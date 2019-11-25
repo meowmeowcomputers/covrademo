d84b4364f97a5d356441b305d845a6cc5bebcdcc
meowmeowcomputers <ryansimonleon@gmail.com>
2019-11-24 18:27:57 -0600

    Admin user and modular refactor plus environment variables

---
+++ app/authenticate.js
@@ -0,0 +1,27 @@
+
+const jwt = require('jsonwebtoken');
+const { User } = require('./models');
+
+// This is the authentication function. It receives a bearer token from the header of the
+// REST request and uses jsonwebtoken to decode the token.
+// The decoded token should return a proper user ID that exists in the database.
+// The function will then assign the user found to req.user for the app to use
+// in additional verification logic.
+const authenticate = async (req, res, next) => {
+  try {
+    let token = req.header('Authorization').replace('Bearer', '').trim();
+    let decoded = jwt.verify(token, 'library-secretkey');
+    let user = await User.findOne({ _id: decoded._id, token });
+    if (!user) {
+      throw new Error();
+    }
+    req.token = token;
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).send({ error: 'Please authenticate!' });
+  }
+};
+
+module.exports = authenticate;
